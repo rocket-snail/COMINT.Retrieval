02055c76183a0402c591e697c516d38f54624983
David Flury <david@flury.email>
2018-11-29 11:24:46 +0100

    Stuff

---
+++ COMINT.Retrieval/COMINT.Retrieval.Common/Models/EngineResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace COMINT.Retrieval.Common.Models
+{
+    [Serializable]
+    class EngineResult
+    {
+        public string Name { get; set; }
+    }
+}
